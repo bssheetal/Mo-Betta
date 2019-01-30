9052765a1eb665c164aa438e19154dc5304a1d54
SheetalSrikumar <bs.sheetal@gmail.com>
2019-01-30 12:54:20 -0800

    post route for faceplus image processing

--- routes/api/faceplus.js
+++ routes/api/faceplus.js
@@ -1,2 +1,25 @@
 const router = require("express").Router();
 const axios = require("axios");
+
+
+var config = {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    responseType: 'json'
+};
+
+var data = {
+    api_key: 'Bks_XP_htVac2PChn4oWlL9v_7ukGWjP',
+    api_secret: '-JQADoPa44FewvVy9VXKq_j5G9jsDICg',
+    image_file: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUVxcaZdoOTdijHWappDS7daG8lhV72iTcrgTh6HPE3yyLCxvR'
+}
+
+router.post("/faceanalyze", (req, res) => {
+    console.log("inside faceanalyze api");
+    axios.post('https://api-us.faceplusplus.com/facepp/v3/detect', data, config).then(response => {
+        res.json(response.data);
+    }).catch(error => {
+        console.log(error);
+    })
+});
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const moodRoutes = require("./stocks");
+const emotionroutes = require("./faceplus");
 
 // Book routes
 router.use("/mobetta", moodRoutes);
+router.use("/faceplusplus",emotionroutes)
 
 module.exports = router;
