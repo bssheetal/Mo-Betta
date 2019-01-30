52fe466078b157d2a3266100c1992008d930ca4a
Iris Cheng <chengiris66@gmail.com>
2019-01-30 14:30:13 -0800

    changes

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use((req, res, next) => {
 app.use(morgan('dev'));
 
 // Setting up express to use json and set it to req.body
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true }, {limit: '50mb'}));
 
 app.use(routes);
 
