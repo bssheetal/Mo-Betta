4ad48c390f6d5b7da1876e9058106a5c9fc2200c
Taylor Walker <taylor.walker@hotmail.com>
2019-01-31 11:32:00 -0800

    spotify function complete

--- routes/api/music.js
+++ routes/api/music.js
@@ -1,23 +1,45 @@
 const router = require("express").Router();
 // const axios = require("axios");
 var Spotify = require('node-spotify-api');
-
 // var spotify = new Spotify(keys.music.id);
 
-router.get("/searchmusic", (req, res) => {
-
-    var spotify = new Spotify({
-        id: '608f7a893412478fb1dffc23cf6bd0c5',
-        secret: '34bf60c1f8e3402294f060254f829845'
-    });
-
-    spotify.search({ type: 'track', query: 'Happy' }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-
-        console.log(data);
-    });
+var spotify = new Spotify({
+    id: '608f7a893412478fb1dffc23cf6bd0c5',
+    secret: '34bf60c1f8e3402294f060254f829845'
 });
 
-module.exports = router;
+
+
+var getMeSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "Happy";
+    }
+
+    spotify.search(
+        {
+            type: "track",
+            query: songName
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
+
+getMeSpotify("Sad");
+
+// module.exports = router;
