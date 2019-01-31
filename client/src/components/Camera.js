279a3c7955ddc9d39a1efd575c889918ebfb9926
Iris Cheng <chengiris66@gmail.com>
2019-01-31 09:13:40 -0800

    changes

--- client/src/components/Camera.js
+++ client/src/components/Camera.js
@@ -35,6 +35,28 @@ class Camera extends Component {
             });
     };
 
+    speechRecognition = () => {
+        // window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+        // let finalTranscript = '';
+        // let recognition = new window.SpeechRecognition();
+        // recognition.interimResults = true;
+        // recognition.maxAlternatives = 10;
+        // recognition.continuous = false;
+        // recognition.onresult = (event) => {
+        //   let interimTranscript = '';
+        //   for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+        //     let transcript = event.results[i][0].transcript;
+        //     if (event.results[i].isFinal) {
+        //       finalTranscript += transcript;
+        //     } else {
+        //       interimTranscript += transcript;
+        //     }
+        //   }
+        //   document.querySelector('body').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
+        // }
+        // recognition.start();
+    };
+
     handleOnClickCapture = e => {
         e.preventDefault();
 
