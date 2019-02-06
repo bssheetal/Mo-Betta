0912ec9ac9e603b719425ba1f88ecfb185f670e9
SheetalSrikumar <bs.sheetal@gmail.com>
2019-02-06 10:47:01 -0800

    saving changes to faceplus id and secret

--- keys.js
+++ keys.js
@@ -3,6 +3,6 @@ exports.stocks={
 }
 
 exports.facerecognition={
-    id:process.env.facerecognition.id,
-    secret:process.env.facerecognition.secret
+    id:process.env.facerecognition_id,
+    secret:process.env.facerecognition_secret
 }
